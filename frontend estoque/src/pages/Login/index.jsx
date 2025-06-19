e804e76bdd168edf6708f2b1046a0737efbacea6
dosdaniel <danieloliveira4551@outlook.com>
2025-06-19 11:06:15 -0300

    ETQ-3 Correcao bug tela login

--- frontend estoque/src/pages/Login/index.jsx
+++ frontend estoque/src/pages/Login/index.jsx
@@ -49,18 +49,18 @@ function Login() {
       }
     } catch (err) {
       console.error(err)
-      setErrorMessage(err.message)
-      // if (err.response) {
-      //   if (typeof err.response.data === 'string') {
-      //     setErrorMessage(err.response.data)
-      //   } else if (err.response.data && err.response.data.error) {
-      //     setErrorMessage(err.response.data.error)
-      //   } else {
-      //     setErrorMessage('Credenciais inválidas ou erro no servidor.')
-      //   }
-      // } else {
-      //   setErrorMessage('Falha de rede ou erro inesperado.')
-      // }
+      // setErrorMessage(err.message)
+      if (err.response) {
+        if (typeof err.response.data === 'string') {
+          setErrorMessage(err.response.data)
+        } else if (err.response.data && err.response.data.error) {
+          setErrorMessage(err.response.data.error)
+        } else {
+          setErrorMessage('Credenciais inválidas ou erro no servidor.')
+        }
+      } else {
+        setErrorMessage('Falha de rede ou erro inesperado.')
+      }
     }
   }
 
